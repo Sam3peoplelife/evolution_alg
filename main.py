9bcbfc0bcd5b9f2ad71689f38f7b0a9999ef7407
Sam3peoplelife <kamishartem1@gmail.com>
2023-05-07 14:14:14 +0200

    first functions

---
+++ main.py
@@ -0,0 +1,32 @@
+import random
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def bukin6(x):
+    return 100 * math.sqrt(abs(x[1] - 0.01 * x[0]**2)) + 0.01 * abs(x[0] + 10)
+
+def matyas(x):
+    return 0.26 * (x[0]**2 + x[1]**2) - 0.48 * x[0] * x[1]
+
+def schaffer2(x):
+    return 0.5 + ((math.sin(x[0]**2 - x[1]**2)**2 - 0.5) / (1 + 0.001 * (x[0]**2 + x[1]**2))**2)
+
+
+def create_ind():
+    return [random.uniform(-5,5), random.uniform(-5,5)]
+
+def create_population(pop_len):
+    population = []
+    for _ in range(pop_len):
+        population.append(create_ind())
+    return population
+
+def fitness_func(population, func):
+    fitness = []
+    for ind in population:
+        fitness.append(func(ind))
+    return fitness
+
+
