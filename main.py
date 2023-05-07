6d1cbbfd3e4b99449ddd0cc9b71e9fe0197050a8
Sam3peoplelife <kamishartem1@gmail.com>
2023-05-07 16:34:46 +0200

    change

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 def bukin6(x):
     return 100 * math.sqrt(abs(x[1] - 0.01 * x[0]**2)) + 0.01 * abs(x[0] + 10)
 
@@ -29,13 +28,13 @@ def fitness_func(population, func):
         fitness.append(func(ind))
     return fitness
 
-def sigma_func(population, sigma):
-    new_population = []
-    for i in range(len(population)):
+def sigma_func(population, sigma, mu):
+    for i in range(mu):
         parent = random.choice(population)
         child = [parent[0] + random.gauss(0, sigma), parent[1] + random.gauss(0, sigma)]
-        new_population.append(child)
-    return new_population
+        population.append(child)
+
+
 
 def mutation(population, mut):
     for i in range(len(population)):
@@ -43,18 +42,24 @@ def mutation(population, mut):
         mutation_candidate = [candidate[0] + random.gauss(0, mut), candidate[1] + random.gauss(0, mut)]
         population[i] = mutation_candidate
 
-def genetic_alg(func, num_gen=1000, pop_len=100, mut_prob=0.1, sigma=0.1):
-    population = create_population(pop_len)
+
+def choose_best(population, fitness, lamda_):
+    new_gen = []
+    fitness.sort()
+    for i in range(lamda_):
+        new_gen.append(population[fitness.index(fitness[lamda_])])
+    return new_gen
+
+def genetic_alg(func, num_gen=100, pop_len=100, mut_prob=0.1, sigma=0.1, lambda_ = 30, mu = 20):
+    population = create_population(lambda_)
+    sigma_func(population, sigma, mu)
     fitness_values = fitness_func(population, func)
     for i in range(num_gen):
-        offspring = sigma_func(population, sigma)
-        mutation(offspring, mut_prob)
-        best = min(fitness_values)
-        avg = sum(fitness_values) / len(fitness_values)
-        if i % 100 == 0:
-            print(i+1, " ", best, " ", avg)
-        population = offspring
-        fitness_values = fitness_func(population, func)
+        print(i+1, " ", min(fitness_values))
+        mutation(population, mut_prob)
+        offspring = choose_best(population, fitness_values, lambda_)
+
+
 
 
 genetic_alg(matyas)
